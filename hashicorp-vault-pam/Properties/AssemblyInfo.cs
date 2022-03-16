b2217b77f31533039865968667c5facc7b966246
Rex Wheeler <rex.wheeler@keyfactor.com>
2022-03-16 12:42:04 -0700

    Add project files.

---
+++ hashicorp-vault-pam/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("VaultPAMProvider")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VaultPAMProvider")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("43cd0f36-3523-442c-a10e-6757d0fba42a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ hashicorp-vault-pam/VaultPAM.cs
@@ -0,0 +1,49 @@
+﻿using Keyfactor.Platform.Extensions;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+
+namespace VaultPAMProvider
+{
+    /*
+    This SQL script will create the Vault PAM Provider Type in Keyfactor
+    Insert Into pam.ProviderTypes VALUES ('46b8e156-4b31-40ad-ad26-4012a5f47177','Vault')
+    Insert Into pam.ProviderTypeParams(ProviderTypeId, Name, DisplayName, DataType, InstanceLevel) VALUES 
+	    ('46b8e156-4b31-40ad-ad26-4012a5f47177','Host','Vault Host',1,0),
+	    ('46b8e156-4b31-40ad-ad26-4012a5f47177','Token','Vault Token',1,0),
+	    ('46b8e156-4b31-40ad-ad26-4012a5f47177','Path','KV Engine Path',1,0),
+    	 
+    	('46b8e156-4b31-40ad-ad26-4012a5f47177','Secret','KV Secret Name',1,1),
+    	('46b8e156-4b31-40ad-ad26-4012a5f47177','Key','KV Secret Key',1,1)
+
+    */
+
+    public class VaultResponseWrapper
+    {
+        public Dictionary<string, string> data;
+    }
+
+    public class VaultPAM : IPAMProvider
+    {
+        public string Name
+        {
+            get
+            {
+                return "Vault";
+            }
+        }
+
+        public string GetPassword(Dictionary<string, string> instanceParameters, Dictionary<string, string> initializationInfo)
+        {
+            WebRequest req = WebRequest.Create($"{initializationInfo["Host"]}{initializationInfo["Path"]}/{instanceParameters["Secret"]}");
+            req.Method = "GET";
+            req.Headers.Add("X-Vault-Token", initializationInfo["Token"]);
+            req.ContentType = "application/json";
+            Stream responseStream = req.GetResponse().GetResponseStream();
+            Dictionary<string, string> response = JsonConvert.DeserializeObject<VaultResponseWrapper>(new StreamReader(responseStream).ReadToEnd()).data;
+
+            return response[instanceParameters["Key"]];
+        }
+    }
+}
