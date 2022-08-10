631f687293704493a5a827c0e910a7d0915d3bd2
Matthew Dobrowsky <11599974+doebrowsk@users.noreply.github.com>
2022-08-10 13:18:21 -0400

    use secret type in config script

--- hashicorp-vault-pam/add_PAMProvider.sql
+++ hashicorp-vault-pam/add_PAMProvider.sql
@@ -6,7 +6,7 @@ values (@pamid, 'Hashicorp-Vault');
 
 insert into [pam].[ProviderTypeParams]([ProviderTypeId], [Name], [DisplayName], [DataType], [InstanceLevel])
 values  (@pamid,'Host','Vault Host',1,0),
-        (@pamid,'Token','Vault Token',1,0),
+        (@pamid,'Token','Vault Token',2,0),
         (@pamid,'Path','KV Engine Path',1,0),
         (@pamid,'Secret','KV Secret Name',1,1),
         (@pamid,'Key','KV Secret Key',1,1);
